4bbfa51949d7c3d67c32cc39ab26c55bda320020
Mohan Krishnan <mohangk@gmail.com>
2016-01-09 12:31:14 +0700

    Adds precedence adjustment tests

---
+++ src/sastrawi/precedence_adjustment.c
@@ -0,0 +1,36 @@
+#ifdef __linux
+  #define _GNU_SOURCE
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../dbg.h"
+
+
+int is_precedence_adjustment_satisfied(char *original_word)
+{
+  int len = strlen(original_word);
+
+  if(strncmp(original_word, "be", 2) == 0) {
+    if(len > 4 && strcmp(&original_word[len-3], "lah") == 0) {
+      return 1;
+    }
+
+    if(len > 3 && strcmp(&original_word[len-2], "an") == 0) {
+      return 1;
+    }
+
+  } else if(strncmp(original_word, "me", 2) == 0 ||
+      strncmp(original_word, "di", 2) == 0 ||
+      strncmp(original_word, "pe", 2) == 0 ||
+      strncmp(original_word, "ter", 3) == 0) {
+
+    if(len > 2 && strcmp(&original_word[len-1], "i") == 0) {
+      return 1;
+    }
+
+  }
+
+  return 0;
+
+}
---
+++ tests/precedence_adjustment_tests.c
@@ -0,0 +1,87 @@
+#ifdef __linux
+  #define _GNU_SOURCE 
+#endif
+#include "minunit.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "libsastrawi.h"
+#include "dbg.h"
+
+char *test_precendence_adjustment_satisfied_be_lah()
+{
+  char *word_to_stem = "belilah";
+  int rc = is_precedence_adjustment_satisfied(word_to_stem);
+  debug("word: %s", word_to_stem);
+  mu_assert(rc == 1, "should be satisfied");
+  return NULL;
+}
+
+char *test_precendence_adjustment_satisfied_be_an()
+{
+  char *word_to_stem = "belaan";
+  int rc = is_precedence_adjustment_satisfied(word_to_stem);
+  debug("word: %s", word_to_stem);
+  mu_assert(rc == 1, "should be satisfied");
+  return NULL;
+}
+
+char *test_precendence_adjustment_satisfied_me_i()
+{
+  char *word_to_stem = "merangkumi";
+  int rc = is_precedence_adjustment_satisfied(word_to_stem);
+  debug("word: %s", word_to_stem);
+  mu_assert(rc == 1, "should be satisfied");
+  return NULL;
+}
+
+char *test_precendence_adjustment_satisfied_di_i()
+{
+  char *word_to_stem = "dikahwini";
+  int rc = is_precedence_adjustment_satisfied(word_to_stem);
+  debug("word: %s", word_to_stem);
+  mu_assert(rc == 1, "should be satisfied");
+  return NULL;
+}
+
+char *test_precendence_adjustment_satisfied_pe_i()
+{
+  char *word_to_stem = "penyanyi";
+  int rc = is_precedence_adjustment_satisfied(word_to_stem);
+  debug("word: %s", word_to_stem);
+  mu_assert(rc == 1, "should be satisfied");
+  return NULL;
+}
+
+char *test_precendence_adjustment_satisfied_ter_i()
+{
+  char *word_to_stem = "terkini";
+  int rc = is_precedence_adjustment_satisfied(word_to_stem);
+  debug("word: %s", word_to_stem);
+  mu_assert(rc == 1, "should be satisfied");
+  return NULL;
+}
+
+char *test_precendence_adjustment_not_satisfied()
+{
+  char *word_to_stem = "terjunam";
+  int rc = is_precedence_adjustment_satisfied(word_to_stem);
+  debug("word: %s", word_to_stem);
+  mu_assert(rc == 0, "should not be satisfied");
+  return NULL;
+}
+
+char *all_tests()
+{
+  mu_suite_start();
+  mu_run_test(test_precendence_adjustment_satisfied_be_lah);
+  mu_run_test(test_precendence_adjustment_satisfied_be_an);
+  mu_run_test(test_precendence_adjustment_satisfied_me_i);
+  mu_run_test(test_precendence_adjustment_satisfied_di_i);
+  mu_run_test(test_precendence_adjustment_satisfied_pe_i);
+  mu_run_test(test_precendence_adjustment_satisfied_ter_i);
+  mu_run_test(test_precendence_adjustment_not_satisfied);
+  return NULL;
+}
+
+RUN_TESTS(all_tests);
