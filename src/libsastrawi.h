bea91545cf01808fe3b21d1dd7bcff63fba0216e
Mohan Krishnan <mohangk@gmail.com>
2016-01-24 16:44:59 +0700

    Initial work on tracking removals

--- src/libsastrawi.h
+++ src/libsastrawi.h
@@ -7,3 +7,10 @@
 #include "sastrawi/dictionary.h"
 
 void print_my_name();
+
+typedef struct affix_removal AFFIX_REMOVAL;
+struct affix_removal {
+  enum {DP, DS, PP, P} type;
+  char *removed_part;
+  char *original_word;
+};
---
+++ tests/affix_removal_tests.c
@@ -0,0 +1,43 @@
+#ifdef __linux
+  #define _GNU_SOURCE 
+#endif
+#include "minunit.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "libsastrawi.h"
+#include "dbg.h"
+
+void set_affix_removal(AFFIX_REMOVAL *affix_removals) {
+  affix_removals[1].removed_part = "blah";
+}
+
+char *test_affix_removal() 
+{
+  AFFIX_REMOVAL affix_removals[3];
+
+  affix_removals[0].removed_part = "kan";
+  affix_removals[0].type = DP;
+  affix_removals[0].original_word = "belikan";
+
+
+  set_affix_removal(affix_removals);
+
+  mu_assert(strcmp(affix_removals[0].original_word,"belikan") == 0, "returns the original word");
+  mu_assert(strcmp(affix_removals[1].removed_part,"blah") == 0, "returns the original word");
+
+  return NULL;
+}
+
+
+
+char *all_tests()
+{
+  mu_suite_start();
+
+  mu_run_test(test_affix_removal);
+
+  return NULL;
+}
+
+RUN_TESTS(all_tests);
