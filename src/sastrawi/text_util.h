4821c62fe8bcd563f6ebab25fe09afe4d9e2d337
William Shallum <william@shallum.net>
2016-02-13 10:42:58 +0700

    add suffix_split_word to text_util.h

--- src/sastrawi/text_util.h
+++ src/sastrawi/text_util.h
@@ -3,4 +3,5 @@
 int split_word(char *pattern, char *word, char **first_part, char **second_part);
 int split_word3(char *pattern, char *word, char **first_part, char **second_part, char *third_part_match);
 int prefix_split_word(char *pattern, char *word, char **first_part, char **second_part);
+int suffix_split_word(char *pattern, char *word, char **first_part, char **second_part);
 #endif
