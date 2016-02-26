0a7cb11a9253d12c5369a96897244e6372961467
William Shallum <william@shallum.net>
2016-02-26 20:46:40 +0700

    internal headers: Add include guards, remove system header includes

--- src/sastrawi/remove_prefixes.h
+++ src/sastrawi/remove_prefixes.h
@@ -1,3 +1,5 @@
+#ifndef SASTRAWI_REMOVE_PREFIXES_H_INCLUDED
+#define SASTRAWI_REMOVE_PREFIXES_H_INCLUDED
 #define NOT_STEMMED 0
 #define PARTIALLY_STEMMED 1
 #define FULLY_STEMMED 2
@@ -26,3 +28,4 @@ int remove_complex_prefix_rule17(char *word, char **stemmed_word, char **removed
 int remove_complex_prefix_rule18(char *word, char **stemmed_word, char **removed_part);
 int remove_complex_prefix_rule19(char *word, char **stemmed_word, char **removed_part);
 int remove_complex_prefix_rule20(char *word, char **stemmed_word, char **removed_part);
+#endif
--- src/sastrawi/stem_singular.h
+++ src/sastrawi/stem_singular.h
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
+#ifndef SASTRAWI_STEM_SINGULAR_H_INCLUDED
+#define SASTRAWI_STEM_SINGULAR_H_INCLUDED
 int stem_singular_word(char *word, char **stemmed_word);
 typedef int (*AFFIX_REMOVER)(char *word, char **stemmed_word);
+#endif
